347fc505f4ea417716f0f5c177f3abfe78f05919
Krishna22v <99374172+Krishna22v@users.noreply.github.com>
2022-02-21 20:53:16 +0530

    Add files via upload

---
+++ Laptop/Laptop1.java
@@ -0,0 +1,11 @@
+package Laptop.com;
+
+public class Laptop1 {
+
+	public void getSystemInfo () {
+		System.out.println("windows10");
+	}
+	
+	
+
+}
---
+++ Laptop/Pc.java
@@ -0,0 +1,12 @@
+package Laptop.com;
+
+		public class Pc extends Laptop1{
+			public void getSystemInfo () {
+				System.out.println("MAC");
+			}
+			public static void main(String[] args) {
+				Pc out=new Pc();
+				out.getSystemInfo();
+	}
+
+}
