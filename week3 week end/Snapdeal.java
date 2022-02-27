63716ef6c42892b4433e6922da3b6a485d190153
Krishna22v <99374172+Krishna22v@users.noreply.github.com>
2022-02-27 20:54:08 +0530

    Add files via upload

---
+++ week3 week end/Snapdeal.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Snapdeal {
+	public static void main(String[] args) {
+		
+	
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver=new ChromeDriver();
+	ChromeOptions option=new ChromeOptions();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	option.addArguments("--disable-notification--");
+	driver.get("https://www.snapdeal.com/");
+	 Alert alert = driver.switchTo().alert();
+  	 alert.accept();
+  	 WebElement men =driver.findElement(By.xpath("//ul[@class='nav smallNav']//span[2]"));
+  	 Actions act=new Actions(driver);
+  	 act.moveToElement(men).perform();
+  	driver.findElement(By.xpath("//span[text()='Sports Shoes']"));
+  	String count =driver.findElement(By.xpath("//span[@class='category-name category-count']")).getTagName();
+  	System.out.println(count);
+	}
+}
