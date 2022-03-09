1f60cd8bec4ccb41496dd5361a8a3b016f89c520
Krishna22v <99374172+Krishna22v@users.noreply.github.com>
2022-03-09 07:30:18 +0530

    Create Babau.java

---
+++ Babau.java
@@ -0,0 +1,13 @@
+class Vowel{
+    public static void main(String[] args) {
+         String str="babau";
+         for( int i = 0;i <a.length();i++)
+         char name=str.charAt(i);
+         switch (name){
+             case 'a':
+             case 'b':
+                 System.out.print(name);
+                 
+         }
+    }
+}
